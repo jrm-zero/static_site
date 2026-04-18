4bfa349159a51ef235d1ab1cf5846ee03766675f
zerosugar <git@jrsort.com>
2026-04-18 08:27:30 -0500

    bold, italics and code markup added

--- src/functions.py
+++ src/functions.py
@@ -17,3 +17,29 @@ def text_node_to_html_node(text_node):
             return LeafNode("img", "", {"src": f"{text_node.url}", "alt": f"{text_node.text}"})
         case _:
             raise Exception(f"{text_node.text_type} does not exist as a type")
+        
+def split_nodes_delimiter(old_nodes, delimiter, text_type):
+    new_nodes = []
+    for node in old_nodes:
+        if node.text_type is not TextType.TEXT:
+            new_nodes.append(node)
+            continue
+        node_split_value = node.text.split(delimiter, maxsplit=2)
+        if len(node_split_value) == 1:
+            new_nodes.append(node)
+            continue
+        if len(node_split_value) != 3:
+            raise Exception("Not Markdown ya dip shit")
+        new_nodes.extend(split_nodes_delimiter(
+            [
+                TextNode(node_split_value[0], TextType.TEXT),
+                TextNode(node_split_value[1], text_type),
+                TextNode(node_split_value[2], TextType.TEXT)
+            ],
+            delimiter,
+            text_type
+        )
+        )
+    return new_nodes
+
+
--- src/test_functions.py
+++ src/test_functions.py
@@ -1,4 +1,5 @@
 import unittest
+import re
 from htmlnode import *
 from textnode import *
 from functions import *
@@ -25,5 +26,50 @@ class TestFunctions(unittest.TestCase):
         html_node = text_node_to_html_node(node)
         self.assertIsNotNone(html_node.props)
         self.assertIn("src", html_node.__repr__())
-    
+    def test_bold(self):
+        node = TextNode("Hello **World**, it is I **Golgeram**!", TextType.TEXT)
+        new_nodes = split_nodes_delimiter([node], "**", TextType.BOLD)
+        self.assertTrue(len(new_nodes) == 5)
+    def test_italics(self):
+        node = TextNode("Hello _World, it is I_ Golgeram!", TextType.TEXT)
+        new_nodes = split_nodes_delimiter([node], "_", TextType.ITALIC)
+        self.assertTrue(len(new_nodes) == 3)
+        for i in range(len(new_nodes)):
+            self.assertNotIn("_", new_nodes[i].text)
+    def test_bold_and_italics(self):
+        node = TextNode("Hello _World, it is I_ a weird **Golgeram**!", TextType.TEXT)
+        new_nodes_2 = split_nodes_delimiter(split_nodes_delimiter([node], "_", TextType.ITALIC), "**", TextType.BOLD)
+        self.assertTrue(len(new_nodes_2) == 5)
+        for i in range(len(new_nodes_2)):
+            self.assertNotIn("_", new_nodes_2[i].text)
+            self.assertNotIn("**", new_nodes_2[i].text)
+    def test_code(self):
+        node = TextNode("Hello World, it is I a weird Golgeram! see my code: `crazy indeed`", TextType.TEXT)
+        new_nodes = split_nodes_delimiter([node], "`", TextType.CODE)
+        self.assertTrue(len(new_nodes) == 3)
+        for i in range(len(new_nodes)):
+            self.assertNotIn("`", new_nodes[i].text)
+    def test_error(self):
+        node = TextNode("Hello World, this is `mess up", TextType.TEXT)
+        with self.assertRaises(Exception) as cm:
+            split_node = split_nodes_delimiter([node], "`", TextType.CODE)
+        the_exception = str(cm.exception)
+        self.assertIn("dip shit", the_exception)
+    def test_link(self):
+        node = TextNode("Hello World, it is I a weird Golgeram! see my image: ![This is an image](url/of/image.jpg) whaaaaattttt", TextType.TEXT)
+        new_nodes = split_nodes_delimiter([node], r"!\[(.*?)\]\((.*?)\)", TextType.CODE)
+        print(new_nodes)
+        print(new_nodes)
+        self.assertTrue(len(new_nodes) == 3)
+        for i in range(len(new_nodes)):
+            self.assertNotIn(r"!\[(.*?)\]\((.*?)\)", new_nodes[i].text)
+    def test_image(self):
+        node = TextNode("Hello World, it is I a weird Golgeram! see my code: `crazy indeed`", TextType.TEXT)
+        new_nodes = split_nodes_delimiter([node], "`", TextType.CODE)
+        self.assertTrue(len(new_nodes) == 3)
+        for i in range(len(new_nodes)):
+            self.assertNotIn("`", new_nodes[i].text)   
+        
+        
+        
 
